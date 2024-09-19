c22ca530ad85a37ff34dc1b3867cbd037aea9e8b
Lucca Emanuelli <137920791+emanuelli01@users.noreply.github.com>
2024-09-18 23:50:25 -0300

    Update exercicio1
    Os pontos aparecem baseados no clique do usuário

--- pythonProject/exercicio1.py
+++ pythonProject/exercicio1.py
@@ -7,43 +7,44 @@ pygame.init()
 
 screen_width = 800
 screen_height = 600
+points = []
 
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
-pygame.display.set_caption('OpenGL da Galera junto com Python')
+pygame.display.set_caption('OpenGL dos dudu')
 
 def init_ortho():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0, 700, 0, 480)
+    gluOrtho2D(0, 800, 0, 600)
 
-done = False
 init_ortho()
 
-def draw_star(x, y, size):
+def drawPoint(x, y, size):          ##função para exibir o ponto
     glPointSize(size)
     glBegin(GL_POINTS)
     glVertex2i(x, y)
     glEnd()
 
+done = False
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+                    ##condição para registrar a posicao do clique quando o usuario pressionar o botao do mouse
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos            ##obtem as coordenadas do clique
+            y = screen_height - y       ### Inverte Y para corresponder ao sistema de coordenadas do OpenGL
+            points.append((x, y))
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-
-    draw_star(231, 151, 20)
-    draw_star(257, 253, 20)
-    draw_star(303, 180, 15)
-    draw_star(443, 228, 20)
-    draw_star(445, 287, 10)
-    draw_star(385, 315, 20)
-    draw_star(371, 343, 10)
-    draw_star(397, 377, 10)
-    draw_star(435, 373, 10)
+    glColor3f(1, 5, 0)
+    ##loop para localizar a coordenada que foi registrada onde o usuario clicou
+    for point in points:          
+        drawPoint(point[0], point[1], 15)     
 
     pygame.display.flip()
     pygame.time.wait(100)
+    
 pygame.quit()
