6bb719a9cbe73259902b7cf62ae7e5b77a7dfe1a
Lucca Emanuelli <137920791+emanuelli01@users.noreply.github.com>
2024-09-19 21:23:41 -0300

    update ex01
    Função para gerar a linha adicionada

--- pythonProject/exercicio1.py
+++ pythonProject/exercicio1.py
@@ -19,10 +19,16 @@ def init_ortho():
 
 init_ortho()
 
-def drawPoint(x, y, size):          ##função para exibir o ponto
+def drawPoint(x, y, size):          ## FUNÇÃO PARA EXIBIR O PONTO
     glPointSize(size)
     glBegin(GL_POINTS)
     glVertex2i(x, y)
+    glEnd()
+
+def drawLine(point1, point2):      ## FUNÇÃO PARA EXIBIR A LINHA
+    glBegin(GL_LINES)
+    glVertex2i(point1[0], point1[1])    ## POINT1 E 2 USADO COMO PARÂMETRO PARA RECEBER A LOCALIZACAO
+    glVertex2i(point2[0], point2[1])    ## ALOCADA NA LISTA POINTS
     glEnd()
 
 done = False
@@ -30,7 +36,7 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-                    ##condição para registrar a posicao do clique quando o usuario pressionar o botao do mouse
+                    ## condição para registrar a posicao do clique quando o usuario pressionar o botao do mouse
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos            ##obtem as coordenadas do clique
             y = screen_height - y       ### Inverte Y para corresponder ao sistema de coordenadas do OpenGL
@@ -40,9 +46,12 @@ while not done:
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     glColor3f(1, 5, 0)
-    ##loop para localizar a coordenada que foi registrada onde o usuario clicou
+    ## LOOP PARA ACESSAR A LOCALIZAÇÃO ONDE O USUARIO CLICOU
     for point in points:          
         drawPoint(point[0], point[1], 15)     
+
+    if len(points) >= 2:
+        drawLine(points[0], points[1])    
 
     pygame.display.flip()
     pygame.time.wait(100)
